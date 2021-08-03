f576608517758874cd1954b0153033c757fd39c3
jveiiga <jeferson.veiiga@icloud.com>
2021-08-03 13:36:42 -0300

    //including API consumption function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+import axios from 'axios'
 import {v4 as uuidv4} from 'uuid';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 
@@ -26,37 +27,50 @@ const App = () => {
     // },
   ]);
 
- 
+  // useEffect(() => {
+  //   const fetchTaks = async () => {
+  //     const { data } = await axios.get(
+  //       "https://jsonplaceholder.cypress.io/todos?_limit-10"
+  //     );
+  //     setTasks(data);
+  //   };
+  //   fetchTaks();
+  // }, []);
 
   const handleTaskClick = (taskId) => {
     const newTasks = tasks.map((task) => {
-      if (task.id === taskId) return {...task, completed: !task.completed}
+      if (task.id === taskId) return { ...task, completed: !task.completed };
 
       return task;
     });
     setTasks(newTasks);
-  }
+  };
 
   const handleTaskAddition = (taskTitle) => {
-    const newTasks = [...tasks, {
-      title: taskTitle,
-      id: uuidv4(),
-      completed: false,
-    },
-  ];
-  setTasks(newTasks);
-  }
+    const newTasks = [
+      ...tasks,
+      {
+        title: taskTitle,
+        id: uuidv4(),
+        completed: false,
+      },
+    ];
+    setTasks(newTasks);
+  };
 
   const handleTaskDeletion = (taskId) => {
-    const newTasks = tasks.filter((task) => task.id !== taskId)
-    setTasks(newTasks)
-  }
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  };
 
   return (
     <Router>
       <div className="container">
         <Header />
-        <Route path="/" exact render={() => (
+        <Route
+          path="/"
+          exact
+          render={() => (
             <>
               <AddInput handleTaskAddition={handleTaskAddition} />
               <Tasks
